ab88127bc2f2f3b248e07ea3de503fa2d64f7fba
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-13 15:54:25 +0300

    add commaClickHandler function

--- src/App.js
+++ src/App.js
@@ -37,6 +37,16 @@ function App() {
     }
   }
 
+  const commaClickHandler = e => {
+    e.preventDefault()
+    const value = e.target.innerHTML
+
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    })
+  }
+
   return (
     <Wrapper>
       <Screen value={calc.num ? calc.num : calc.res} />
