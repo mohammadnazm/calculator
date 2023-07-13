f011fe2aa9d21fa0a0d597f42531fcd77dff9cea
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-13 12:06:00 +0300

    Define States

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react"
 import Wrapper from "./components/Wrapper"
 import Screen from "./components/Screen"
 import ButtonBox from "./components/ButtonBox"
@@ -12,6 +13,12 @@ const btnValues = [
 ]
 
 function App() {
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  })
+
   return (
     <Wrapper>
       <Screen value="0" />
