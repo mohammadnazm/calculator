7c5f6702f1bee0be09a97e7668483498778f6323
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-07-13 15:29:25 +0300

    add functionality

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <Wrapper>
-      <Screen value="0" />
+      <Screen value={calc.num ? calc.num : calc.res} />
       <ButtonBox>
         {btnValues.flat().map((btn, i) => {
           return (
@@ -29,9 +29,21 @@ function App() {
               key={i}
               className={btn === "=" ? "equals" : ""}
               value={btn}
-              onClick={() => {
-                console.log(`${btn} clicked!`)
-              }}
+              onClick={
+                btn === "C"
+                  ? resetClickHandler
+                  : btn === "+-"
+                  ? invertClickHandler
+                  : btn === "%"
+                  ? percentClickHandler
+                  : btn === "="
+                  ? equalsClickHandler
+                  : btn === "/" || btn === "X" || btn === "-" || btn === "+"
+                  ? signClickHandler
+                  : btn === "."
+                  ? commaClickHandler
+                  : numClickHandler
+              }
             />
           )
         })}
